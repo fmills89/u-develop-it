f636c2792631679c887604dec6c6d762f87e0c43
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-01-19 17:16:20 -0600

    created party table and new api routes to get/delete/put info

--- server.js
+++ server.js
@@ -65,6 +65,83 @@ app.get('/api/candidate/:id', (req, res) => {
     });
 });
 
+app.get('/api/parties', (req, res) =>{
+    const sql = `select * from parties`;
+    db.query(sql, (err, rows) => {
+        if(err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+app.get('/api/party/:id', (req, res) =>{
+    const sql = `select * from parties where id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) =>{
+        if (err) {
+            res.status(400).json({ error: err.messge });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+app.put('/api/candidate/:id', (req, res) =>{
+    const errors = inputCheck(req.body, 'party_id');
+    
+    if(errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `update candidates set party_id = ?
+                where id = ?`;
+    const params = [req.body.party_id, req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
+
+
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `delete from parties where id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.messge });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Party not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
 // Delete a candidate
 // ? denotes a placeholder, making this a prepared statement
 app.delete('/api/candidate/:id', (req, res) => {
