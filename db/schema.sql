633b54a61137c96cc80b342b13c00595a7a66b17
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-01-20 09:27:40 -0600

    added voters table and data

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
+drop table if exists voters;
 
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -14,6 +15,14 @@ CREATE TABLE candidates (
     party_id integer,
     industry_connected BOOLEAN NOT NULL,
     CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
+
+create table voters (
+  id integer AUTO_INCREMENT PRIMARY KEY,
+  first_name varchar(30) not null,
+  last_name varchar(30) not null,
+  email varchar(50) not null,
+  created_at datetime default current_timestamp
 );
 
 
